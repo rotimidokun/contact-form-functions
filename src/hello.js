df96a2849232ef421ba94dc2b6ad8e3c7f00f9aa
rotimidokun <51387182+rotimidokun@users.noreply.github.com>
2020-12-28 20:20:58 +0100

    hello.js
    Initial test email setup

--- src/hello.js
+++ src/hello.js
@@ -3,6 +3,30 @@ import nodemailer from "nodemailer"
 
 exports.handler = async (event, context) => {
 
+let testAccount = await nodemailer.createTestAccount()
+
+let transporter = nodemailer.createTransport({
+host: 'smtp.ethereal.email',
+port: 587,
+secure: false,
+auth: {
+  user: testAccount.user,
+  pass: testAccount.pass,
+}
+})
+
+let info = await transporter.sendMail({
+  from: 'sender@example.com',
+  to: 'rotimidokun@gmail.com',
+  subject: 'subject_sample',
+  text: 'text_sample',
+})
+
+console.log('Message sent: %s', info.messageId)
+console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info))
+
+
   return { statusCode: 200  }
 
 };
+
